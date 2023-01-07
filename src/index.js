8fb6d3aa0cb61df58400ccfeabb5d0c57fc7f35d
Leigh Michael Forrest <leigh@catbirdseat.io>
2023-01-07 09:49:11 -0500

    js message added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import multiple from "multiple.js";
-
-const multiple_js = multiple({
-    selector: '.item',
-    background: 'linear-gradient(#273463, #8B4256)'
-  });
+console.log("Hello World!");
